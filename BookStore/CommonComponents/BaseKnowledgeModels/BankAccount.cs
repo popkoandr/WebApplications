276765e21e8db8d235599eafa2277f755866cad6
Andrii Popko <andrew.popko.j@gmail.com>
2018-07-21 23:48:46 +0300

    last checking on the old NB

---
+++ BookStore/CommonComponents/BaseKnowledgeModels/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CommonComponents.BaseKnowledgeModels
+{
+    public class BankAccount
+    {
+
+
+        int _sum; // Переменная для хранения суммы
+
+        // Объявляем делегат
+        public delegate void AccountStateHandler(string message);
+        // Создаем переменную делегата
+        AccountStateHandler _del;
+
+        //public void RegisterHandler(AccountStateHandler del)
+        //{
+        //    _del = del;
+        //}
+        // Регистрируем делегат
+        public void RegisterHandler(AccountStateHandler del)
+        {
+            Delegate mainDel = System.Delegate.Combine(del, _del);
+            _del = mainDel as AccountStateHandler;
+        }
+        // Отмена регистрации делегата
+        public void UnregisterHandler(AccountStateHandler del)
+        {
+            Delegate mainDel = System.Delegate.Remove(_del, del);
+            _del = mainDel as AccountStateHandler;
+        }
+
+        public BankAccount(int sum)
+        {
+            _sum = sum;
+        }
+
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum <= _sum)
+            {
+                _sum -= sum;
+
+                _del?.Invoke($"Сумма {sum} снята со счета");
+            }
+            else
+            {
+                _del?.Invoke("Недостаточно денег на счете");
+            }
+            
+        }
+    }
+}
---
+++ BookStore/CommonComponents/Helpers/BaseKnowledgeHepler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CommonComponents.Helpers
+{
+    public class BaseKnowledgeHepler
+    {
+        //private delegate string GetMessage(string name);
+
+        //private static string GetGreetingsMessage(GetMessage delGetMessage, string name)
+        //{
+        //   return delGetMessage(name);
+        //}
+
+        //private static string SayGoodMorning(string name)
+        //{
+        //    return $"Hello {name}";
+        //}
+
+        //private static string SayHello(string name)
+        //{
+        //    return ($"Hello {name}");
+        //}
+        //public static void SayGreeting()
+        //{
+        //    if (DateTime.Now.Hour < 12)
+        //    {
+        //        MessageBox.Show (GetGreetingsMessage(SayGoodMorning, "Andrew"));
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show(GetGreetingsMessage(SayHello, "Andrew"));
+        //    }
+        //    Console.ReadLine();
+        //}
+
+    }
+}
